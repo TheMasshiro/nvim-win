5c0f0fc7c0ec70216dc27bc5fa35add8a21bd679
John Christian Vicente <johnc.vicente1@gmail.com>
2025-02-11 21:59:15 +0800

    yanked something from lazyvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,26 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {},
 	dependencies = {
-		"MunifTanjim/nui.nvim",
+		{ "MunifTanjim/nui.nvim", lazy = true },
 	},
+	opts = {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			bottom_search = true,
+			long_message_to_split = true,
+		},
+	},
+	config = function(_, opts)
+		if vim.o.filetype == "lazy" then
+			vim.cmd([[messages clear]])
+		end
+		require("noice").setup(opts)
+	end,
 }
