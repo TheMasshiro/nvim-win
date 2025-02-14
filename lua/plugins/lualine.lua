bad246f7d6f9f63d0ea5a4d9ce21f049e9d66f38
John Christian Vicente <johnc.vicente1@gmail.com>
2025-02-14 11:29:40 +0800

    Added lualine again

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			theme = "auto",
+			section_separators = { left = "█", right = "█" },
+			component_separators = { left = "|", right = "|" },
+			disabled_filetypes = {
+				statusline = { "snacks_dashboard", "snacks_picker_list", "trouble" },
+			},
+		},
+	},
+}
