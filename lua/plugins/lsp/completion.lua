de6367cb021b0ee3318159711fca1212bd2ad13b
John Christian Vicente <johnc.vicente1@gmail.com>
2025-03-10 12:55:22 +0800

    blink.cmp new update
    blink ftw

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -13,6 +13,7 @@ return {
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
 }
