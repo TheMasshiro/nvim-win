679dc0b36ada53493266adc974bd8b9aebd2c61d
John Christian Vicente <johnc.vicente1@gmail.com>
2025-04-08 18:43:04 +0800

    made screenkey to run upon opening nvim

--- lua/plugins/utils/screenkey.lua
+++ lua/plugins/utils/screenkey.lua
@@ -1,21 +1,18 @@
 return {
 	"NStefan002/screenkey.nvim",
+	lazy = false,
 	version = "*",
-	cmd = "Screenkey",
-	keys = {
-		{
-			"\\bk",
-			"<cmd>Screenkey toggle<cr>",
-			desc = "Toggle Screenkey",
-		},
-	},
-	opts = {
-		win_opts = {
-			col = vim.o.columns,
-			width = 30,
-			border = "none",
-			title = "",
-		},
-		show_leader = false,
-	},
+	config = function()
+		require("screenkey").setup({
+			win_opts = {
+				col = vim.o.columns,
+				width = 30,
+				border = "none",
+				title = "",
+			},
+			show_leader = false,
+			group_mappings = true,
+		})
+		require("screenkey").toggle()
+	end,
 }
