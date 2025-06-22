e26da56b97da28520fa445936e62be7c85848605
John Christian Vicente <johnc.vicente1@gmail.com>
2025-06-22 21:00:14 +0800

    fix(lsp/css): Add `--stdio` flag to CSS language server command

--- lsp/css.lua
+++ lsp/css.lua
@@ -1,6 +1,6 @@
 local mason_path = require("config.paths").nvim.mason
 
 return {
-    cmd = { mason_path .. "vscode-css-language-server.cmd" },
-    filetypes = { "css", "sass", "scss", "less" },
+	cmd = { mason_path .. "vscode-css-language-server.cmd", "--stdio" },
+	filetypes = { "css", "sass", "scss", "less" },
 }
